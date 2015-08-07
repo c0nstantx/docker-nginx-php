6946a4ebef27177142e3f4597ba2ceb632fe4b8d
Konstantinos Christofilos <kostas.christofilos@rocketgraph.com>
2015-08-07 09:55:45 +0300

    Change URI to IP

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -x
 #echo "starting supervisor in foreground"
-if [ ! -z "$LOGSTASH_URI" ] && [ ! -z "$LOGSTASH_HOST" ]; then
-    echo "${LOGSTASH_URI} ${LOGSTASH_HOST}" >> /etc/hosts
+if [ ! -z "$LOGSTASH_IP" ] && [ ! -z "$LOGSTASH_HOST" ]; then
+    echo "${LOGSTASH_IP} ${LOGSTASH_HOST}" >> /etc/hosts
 fi
 supervisord -n
